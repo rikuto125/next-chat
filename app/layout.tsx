1cd50f763f97d36b1cfcbcbf19e37c4b8a63d470
iwairikuto <wjrikuto@gmial.com>
2023-05-27 09:26:58 +0900

    AuthContext追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import React from "react";
-import ToasterContext from "@/app/context/ToasterContext";
+import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   );
